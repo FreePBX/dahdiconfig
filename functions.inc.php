cc136cbc6f7a9dc9488776acc603c66bffbe9547
James Finstrom <james.finstrom@schmoozecom.com>
2015-01-19 18:27:05 -0300

    Fix hook to core for bootstrap

--- functions.inc.php
+++ functions.inc.php
@@ -482,43 +482,67 @@ function dahdiconfig_hook_core($viewing_itemid, $target_menuid) {
     $avail_trunks = dahdiconfig_get_unused_trunk_options($_REQUEST['dahdi_current_channel']);
     if (!empty($avail_trunks)) {
       $html = '
-				<tr>
-					<td>
-						<a href=# class="info">' . _("DAHDI Trunks") . '<span>' . _("Available DAHDI Groups and Channels configued in the DAHDI Configuration Module") . '</span></a>:
-          </td>
-          <td>
-            <select name="dahdi_trunks" id="dahdi_trunks" tabindex="' . ++$tabindex . '">
-      ';
-      foreach ($avail_trunks as $ident) {
-        $selected = $ident['selected'] ? ' SELECTED' : '';
-        $html .= "
-            <option value='{$ident['identifier']}'$selected>{$ident['name']}
-        ";
+      <!--DAHDICONFIG HOOK-->
+      <div class="element-container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="form-group">
+                <div class="col-md-3">
+                  <label class="control-label" for="dahdi_trunks"><?php echo _("DAHDI Trunks") ?></label>
+                  <i class="fa fa-question-circle fpbx-help-icon" data-for="dahdi_trunks"></i>
+                </div>
+                <div class="col-md-9">
+                  <select name="dahdi_trunks" id="dahdi_trunks"  class="form-control" tabindex="' . ++$tabindex . '">';
+                  foreach ($avail_trunks as $ident) {
+                   $selected = $ident['selected'] ? ' SELECTED' : '';
+                    $html .= "<option value='{$ident['identifier']}'$selected>{$ident['name']}</option>";
       }
-      $html .= "
-            </select>
-					</td>
-        </tr>
-      ";
+      $html .= '
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <span id="dahdi_trunks-help" class="help-block fpbx-help-block"><?php echo _("Available DAHDI Groups and Channels configued in the DAHDI Configuration Module")?></span>
+          </div>
+        </div>
+      </div>
+      <!--END DAHDICONFIG HOOK-->
+      ';
     } else {
       $html .= '
-				<tr>
-					<td colspan="2">
-						<a href=# class="info">' . _("No Available Groups or Channels") . '<span>' . _("There are no DAHDI Groups or Channels available to be configured. Check the DAHDI module (linked below) to configure any un-used cards") . '</span></a>
-          </td>
-				</tr>
+        <!--DAHDICONFIG HOOK-->
+        <div class="element-container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="row">
+                <div class="form-group">
+                  <div class="col-md-3">
+                    <label class="control-label" for="dahdi_trunks"><?php echo _("DAHDI Trunks") ?></label>
+                    <i class="fa fa-question-circle fpbx-help-icon" data-for="dahdi_trunks"></i>
+                  </div>
+                  <div class="col-md-9">
+                    <b><?php echo _("No Available Groups or Channels")?></b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <span id="dahdi_trunks-help" class="help-block fpbx-help-block"><?php echo _("There are no DAHDI Groups or Channels available to be configured. Check the DAHDI module (linked below) to configure any un-used cards")?></span>
+            </div>
+          </div>
+        </div>
+        <!--END DAHDICONFIG HOOK-->
       ';
     }
 
 	  $URL = '?'.'display=dahdi';
-    $html .= '
-				<tr><td colspan="2"><input type="hidden" id="dahdi_trunks" value=""></td></tr>
-				<tr>
-					<td colspan="2">
-						<a href="'.$URL.'" class="info">' . _("Configure/Edit DAHDI Cards") . '<span>' . _("Configure/Edit DAHDI Card settings in DAHDi Module") . '</span></a>
-          </td>
-				</tr>
-    ';
   }
   return $html;
 }

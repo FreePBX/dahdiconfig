10b4b70b3b74324e0e9572fcf462840aaaa40860
James Finstrom <jfinstrom@sangoma.com>
2015-12-08 10:29:10 -0700

    FREEPBX-10898 array validation

--- views/dahdi_digital_settings.php
+++ views/dahdi_digital_settings.php
@@ -202,6 +202,7 @@
     <h2><?php echo ('Group Settings')?> (<a style="cursor:pointer;" onclick="reset_digital_groups(<?php echo $key;?>,<?php echo $span['totchans']-1?>);"><?php echo ('Reset Groups')?></a>)</h2>
     <hr>
     <?php $groups = json_decode($span['additional_groups'],TRUE);
+        $groups = is_array($groups)?$groups:array();
         foreach($groups as $gkey => $data) {
     ?>
     <table width="100%" id="editspan_<?php echo $key?>_group_settings_<?php echo $gkey?>" style="text-align:left;" border="0" cellspacing="0">

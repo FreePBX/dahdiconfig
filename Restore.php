acfbe6841cf6f93220c11e79720a0e51e2b49015
James Finstrom <jfinstrom@sangoma.com>
2018-07-17 18:06:03 +0000

    Restore cleanup

--- Restore.php
+++ Restore.php
@@ -1,25 +1,25 @@
 <?php
-namespace FreePBX\modules\__MODULENAME__;
+namespace FreePBX\modules\Dahdiconfig;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
 
-    public function runRestore($jobid){
-        $configs = $this->getConfigs();
-        foreach ($configs as $key => $value) {
-            $this->FreePBX->Config->update($key,$value);
-        }
-        $files = $this->getFiles();
-        foreach ($files as $file) {
-            $filename = $file['pathto'] . '/' . $file['filename'];
-            if(file_exists($filename)){
-                rename($filename, $filename.'.bak');
-            }
-            copy($this->tmpdir . '/files/' . $filename, $filename);
-        }
-        //Let the module do what it needs to do.
-        $dahdiCards = new \dahdi_cards();
-        $dahdiCards->load();
-        $dahdiCards->checkHardware();
-    }
-    
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		foreach ($configs as $key => $value) {
+			$this->FreePBX->Config->update($key,$value);
+		}
+		$files = $this->getFiles();
+		foreach ($files as $file) {
+			$filename = $file['pathto'] . '/' . $file['filename'];
+			if(file_exists($filename)){
+				rename($filename, $filename.'.bak');
+			}
+			copy($this->tmpdir . '/files' . $filename, $filename);
+		}
+		//Let the module do what it needs to do.
+		$dahdiCards = new \dahdi_cards();
+		$dahdiCards->load();
+		$dahdiCards->checkHardware();
+	}
+	
 }

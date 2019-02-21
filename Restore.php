ae4880b72b72795b3f96455feea12c23bec909bc
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 15:04:08 -0500

    [Module Tag script: dahdiconfig 15.0.5] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -8,24 +8,9 @@ class Restore Extends Base\RestoreBase{
 		foreach ($configs as $key => $value) {
 			$this->FreePBX->Config->update($key,$value);
 		}
-		$files = $this->getFiles();
-		foreach ($files as $file) {
-			$filename = $file['pathto'] . '/' . $file['filename'];
-			if(file_exists($filename)){
-				rename($filename, $filename.'.bak');
-			}
-			copy($this->tmpdir . '/files' . $filename, $filename);
-		}
-		//Let the module do what it needs to do.
-		$dahdiCards = new \dahdi_cards();
-		$dahdiCards->load();
-		$dahdiCards->checkHardware();
 	}
-	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-		$time = time();
-		$ampconf = $this->getAMPConf($pdo);
-
-		$advancedSettings = [
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$advanced = [
 			'DAHDIEXEC',
 			'DAHDIMODULESLOC',
 			'DAHDIDISABLEWRITE',
@@ -35,47 +20,12 @@ class Restore Extends Base\RestoreBase{
 			'DAHDIMOCKHW',
 			'ZAP2DAHDICOMPAT',
 		];
-		foreach ($advancedSettings as $key) {
-			if(isset($ampconf[$key])){
-				$this->FreePBX->Config->update($key, $ampconf[$key]);
-			}
-		}
-		$dahdidir = $tmpfiledir . '/etc/dahdi';
-		if(is_dir($dahdidir)){
-			$iterator = new \DirectoryIterator($dahdidir);
-			foreach ($iterator as $fileinfo) {
-				if($fileinfo->isFile() && $fileinfo->isReadable()){
-					if(!is_dir('/etc/dahdi')){
-						mkdir('/etc/dahdi', 0755);
-					}
-					$filename = $dahdidir.'/'. $fileinfo->getFilename();
-					copy($filename, '/etc/dahdi/' . $fileinfo->getFilename());
-					if (is_file('/etc/dahdi/' . $fileinfo->getFilename())) {
-						copy('/etc/dahdi/' . $fileinfo->getFilename(), '/etc/dahdi/' . $fileinfo->getFilename() . '.' . $time);
-					}
-				}
-			}
-		}
-		$wanpipedir = $tmpfiledir . '/etc/wanpipe';
-		if(is_dir($wanpipedir)){
-			$iterator = new \DirectoryIterator($wanpipedir);
-			foreach ($iterator as $fileinfo) {
-				if($fileinfo->isFile() && $fileinfo->isReadable()){
-					if (!is_dir('/etc/wanpipe')) {
-						mkdir('/etc/wanpipe', 0755);
-					}
-					$filename = $wanpipedir.'/'. $fileinfo->getFilename();
-					if(is_file('/etc/wanpipe/' . $fileinfo->getFilename())){
-						copy('/etc/wanpipe/' . $fileinfo->getFilename(), '/etc/dahdi/' . $fileinfo->getFilename().'.'.$time);
-					}
-					copy($filename,'/etc/dahdi/'.$fileinfo->getFilename());
-				}
+		foreach ($advanced as $key) {
+			if(isset($data['settings'][$key])){
+				$this->FreePBX->Config->update($key, $data['settings'][$key]);
 			}
 		}
 
-		$dahdiCards = new \dahdi_cards();
-		$dahdiCards->load();
-		$dahdiCards->checkHardware();
 		return $this;
 	}
 }

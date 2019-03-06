007c6c9b664fa16e99d7aa81e15881b13dbff2bf
Andrew Nagy <andrew.nagy@the159.com>
2019-03-06 14:54:19 -0500

    fix legacy restore

--- Backup.php
+++ Backup.php
@@ -6,13 +6,6 @@ use SplFileInfo;
 class Backup Extends Base\BackupBase{
 	public function runBackup($id,$transaction){
 		/** Everything is dynamic so ummm so we simply save the advanced settings ¯\_(ツ)_/¯ */
-		$this->addConfigs($this->getAdvanced());
+		$this->addConfigs($this->dumpAdvancedSettings());
 	}
-
-	public function getAdvanced(){
-		$all = $this->FreePBX->Config->get_conf_settings();
-		return array_filter($all, function($settings){
-			return (isset($settings['module']) && $settings['module'] == 'dahdiconfig' && isset($settings['modified']) && $settings['modified']);
-		});
-}
 }
--- Restore.php
+++ Restore.php
@@ -5,27 +5,9 @@ class Restore Extends Base\RestoreBase{
 
 	public function runRestore($jobid){
 		$configs = $this->getConfigs();
-		foreach ($configs as $key => $value) {
-			$this->FreePBX->Config->update($key,$value);
-		}
+		$this->importAdvancedSettings($configs);
 	}
 	public function processLegacy($pdo, $data, $tables, $unknownTables){
-		$advanced = [
-			'DAHDIEXEC',
-			'DAHDIMODULESLOC',
-			'DAHDIDISABLEWRITE',
-			'DAHDIMODPROBELOC',
-			'DAHDIECHOCAN',
-			'DAHDISYSTEMLOC',
-			'DAHDIMOCKHW',
-			'ZAP2DAHDICOMPAT',
-		];
-		foreach ($advanced as $key) {
-			if(isset($data['settings'][$key])){
-				$this->FreePBX->Config->update($key, $data['settings'][$key]);
-			}
-		}
-
-		return $this;
+		$this->restoreLegacySettings($pdo);
 	}
 }
